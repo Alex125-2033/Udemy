f371259c32b608a6ecb304f903df1826889575bc
Alex Klychkov <nicealeksandr48@gmail.com>
2025-12-04 15:38:21 +0400

    first work

---
+++ text/csript.js
@@ -0,0 +1,19 @@
+"use sctrict"
+const number0FFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const  personalMovieDB = {
+    count : number0FFilms,
+    movies: {},
+    actors: {},
+    genders: [],
+    privat: false
+};
+const a = prompt("Один из последних просмотренных фильмов?"),
+      b = prompt("Насколько вы его оцените?"),
+      c = prompt("Один из последних просмотренных фильмов?"),
+      d = prompt("Насколько вы его оцените?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
